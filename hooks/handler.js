e5cd9277ed9e497642c522fb9b596f679afe1abf
Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>
2020-12-16 10:58:49 +0000

    Add initial tweet function
    Signed-off-by: Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>

--- hooks/handler.js
+++ hooks/handler.js
@@ -3,8 +3,8 @@
 var fs = require("fs");
 
 module.exports = async (event, context) => {
-  const res = fs.readFileSync("/var/openfaas/secrets/secret", "utf8")
-
+//  const res = fs.readFileSync("/var/openfaas/secrets/secret", "utf8")
+  let res ="Hello!"
   return context
     .status(200)
     .succeed(res)
---
+++ save-tweet/handler.js
@@ -0,0 +1,34 @@
+'use strict'
+const TweetFetch = require("tweet-fetch")
+const fsPromises = fs.promises
+const fs = require('fs')
+
+module.exports = async (event, context) => {
+  if(!event.query['q']) {
+    return context.headers({'Content-type': 'text/plain', "Access-Control-Allow-Origin": "http://localhost:8000"})
+            .status(400)
+            .fail('Give a Twitter URL with ?q=https://twitter.com/alexellisuk/status/1338979828746358790?s=20')
+  }
+
+  let consumerKey = await fsPromises.readFile('/var/openfaas/secrets/consumer-key', 'utf8')
+  let consumerSecret = await fsPromises.readFile('/var/openfaas/secrets/consumer-secret', 'utf8')
+  let accessTokenKey = await fsPromises.readFile('/var/openfaas/secrets/access-token-key', 'utf8')
+  let accessTokenSecret = await fsPromises.readFile('/var/openfaas/secrets/access-token-secret', 'utf8')
+
+  
+  let tweetURL = event.query['q'].toLowerCase()
+  
+  const tweetFetch = new TweetFetch({
+    consumer_key: consumerKey,
+    consumer_secret: consumerSecret,
+    access_token_key: accessTokenKey,
+    access_token_secret: accessTokenSecret,
+  })
+  
+  let res = await tweetFetch.get(tweetURL)  
+
+  return context
+    .status(200)
+    .succeed(res)
+}
+
