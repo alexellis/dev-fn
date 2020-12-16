eef2b86da7c7eb2c8a4fd6a1e084d2b460b0d553
Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>
2020-12-16 11:44:14 +0000

    Test canvas
    Signed-off-by: Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>

--- image-test/handler.js
+++ image-test/handler.js
@@ -1,12 +1,26 @@
 'use strict'
+const { createCanvas } = require('canvas')
 
-module.exports = async (event, context) => {
+module.exports = async (event, ctx) => {
   const result = {
     'status': 'Received input: ' + JSON.stringify(event.body)
   }
 
-  return context
+  const context = canvas.getContext('2d')
+
+  const text = 'Hello, OpenFaaS!'
+  context.textBaseline = 'top'
+  context.fillStyle = '#3574d4'
+  const textWidth = context.measureText(text).width
+  context.fillRect(600 - textWidth / 2 - 10, 170 - 5, textWidth + 20, 120)
+  context.fillStyle = '#fff'
+  context.fillText(text, 600, 170)
+
+  const buffer = canvas.toBuffer('image/png')
+
+  return ctx
     .status(200)
-    .succeed(result)
+    .headers({"Content-Type": "image/png"})
+    .succeed(buffer)
 }
 
